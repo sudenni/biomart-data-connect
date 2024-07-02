b605230586855110f8319b152f316fb6ef250296
Sukanya Denni <sukanya@ebi.ac.uk>
2024-07-02 15:10:52 +0100

    renamed routes to pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 from app.config import Config
-from app import routes
+from app import pages
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.register_blueprint(routes.app)
+    app.register_blueprint(pages.app)
     return app
