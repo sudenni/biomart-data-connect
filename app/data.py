c9029170ea5d7c44db2a43ad41a1f8276370baa7
Sukanya Denni <sukanya@ebi.ac.uk>
2024-07-03 11:12:08 +0100

    add limit and where statement to SQL query

--- app/data.py
+++ app/data.py
@@ -1,4 +1,4 @@
-from pypika import PostgreSQLQuery
+from pypika import PostgreSQLQuery, Criterion
 from search_python_client.search import SearchClient
 
 from flask import current_app as app
@@ -21,15 +21,33 @@ class DataConnectConnection():
         return list(d.keys())
 
 class BuildSQLQuery():
-    def __init__(self, table, cols) -> None:
+    def __init__(self, table, cols, filters = None, limit = 5, distinct = False) -> None:
         catalog = app.config["CATALOG"]
         schema = app.config["SCHEMA"]
         self.table = table(schema=(catalog, schema))
         self.cols = cols
-    
+        self.filters = filters
+        self.limit = limit
+        self.distinct = distinct
+
     def build_base_query(self):
         query = PostgreSQLQuery.from_(self.table).select(*(self.table[col] for col in self.cols))
+        if self.filters is not None:
+            query = self.add_filter(query)
+        if self.limit is not None:
+            query = query.limit(self.limit)
+        if self.distinct:
+            query = query.distinct()
         return query
     
-    def add_filter(self):
-        pass
+    def add_filter(self, query):
+        conds = []
+        t = self.table
+        for element in self.filters:
+            print(element)
+            for key, value in element:
+                condition_field = getattr(t, key)
+                conds.append(condition_field == value)
+        # AND
+        condition = Criterion.all([x for x in conds])
+        return query.where(condition)
